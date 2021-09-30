ac9ee9c9d9e8eb79d538aa7da054fe588d8f47c0
unknown <farhanzuhair040703@gmail.com>
2021-09-30 12:20:47 +0700

    Jurnal1.java

---
+++ Jurnal1.java
@@ -0,0 +1,8 @@
+public class Jurnal1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Nama : Muhammad Farhan Zuhair");
+		System.out.println("NIM : 6706213054");
+		System.out.println("Kelas : D3RPLA 45-05");
+	}
+}
